a42a67d2e3b1e3cda0b97fdba65600c5d15d9d7e
KamillaP3 <kparker3@ualr.edu>
2024-08-21 15:52:17 +0000

    save

---
+++ 01.05 Previous and Next.py
@@ -0,0 +1,6 @@
+enternumber = input("Enter Number: ")
+enternumber = int(enternumber)
+numberbefore = enternumber- 1
+numberafter = enternumber + 1
+print("The next number for the number " + str(enternumber) + " is " + str(numberafter))
+print("The previous number for the number " + str(enternumber) + " is " + str(numberbefore))
