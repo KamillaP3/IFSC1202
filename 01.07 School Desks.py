a03cc9aa94334f1f4689c17e919daca03b6588bc
KamillaP3 <kparker3@ualr.edu>
2024-08-22 21:18:00 +0000

    save

--- 01.07 School Desks.py
+++ 01.07 School Desks.py
@@ -1,15 +1,19 @@
-numberofstudentsA = input("Enter Number of Hours in Classroom A: ")
-numberofstudentsB = input("Enter Number of Hours in Classroom B: ")
-numberofstudentsC = input("Enter Number of Hours in Classroom C: ")
+numberofstudentsA = input("Enter Number of Students in Classroom A: ")
+numberofstudentsB = input("Enter Number of Students in Classroom B: ")
+numberofstudentsC = input("Enter Number of Students in Classroom C: ")
 
-numberofdesksA= int(numberofstudentsA) // 2
-numberofdesksB= int(numberofstudentsB) // 2
-numberofdesksC= int(numberofstudentsC) // 2
+numberofdesksA = int(numberofstudentsA) // 2
+numberofdesksB = int(numberofstudentsB) // 2
+numberofdesksC = int(numberofstudentsC) // 2
 
-deskonestudentA= int(numberofstudentsA) % 2
-deskonestudentB= int(numberofstudentsB) % 2
-deskonestudentC= int(numberofstudentsC) % 2
+deskonestudentA = int(numberofstudentsA) % 2
+deskonestudentB = int(numberofstudentsB) % 2
+deskonestudentC = int(numberofstudentsC) % 2
 
-addedonedeskAll= deskonestudentA + deskonestudentB + deskonestudentC)
-numberofdesksAllfull= numberofdesksA + numberofdesksB + numberofdesksC
+addedonedeskAll = deskonestudentA + deskonestudentB + deskonestudentC
+numberofdesksAllfull = numberofdesksA + numberofdesksB + numberofdesksC
 newaddedfulldeskAll = addedonedeskAll // 2
+newaddedonedeskAll = addedonedeskAll % 2
+
+totaldesks = str(newaddedonedeskAll + newaddedfulldeskAll + numberofdesksAllfull)
+print(str(totaldesks))
