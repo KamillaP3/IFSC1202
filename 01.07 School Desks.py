ce3ecee1db3e57e04fc40b270089385204ed7e45
KamillaP3 <kparker3@ualr.edu>
2024-08-22 17:30:50 +0000

    save

---
+++ 01.07 School Desks.py
@@ -0,0 +1,15 @@
+numberofstudentsA = input("Enter Number of Hours in Classroom A: ")
+numberofstudentsB = input("Enter Number of Hours in Classroom B: ")
+numberofstudentsC = input("Enter Number of Hours in Classroom C: ")
+
+numberofdesksA= int(numberofstudentsA) // 2
+numberofdesksB= int(numberofstudentsB) // 2
+numberofdesksC= int(numberofstudentsC) // 2
+
+deskonestudentA= int(numberofstudentsA) % 2
+deskonestudentB= int(numberofstudentsB) % 2
+deskonestudentC= int(numberofstudentsC) % 2
+
+addedonedeskAll= deskonestudentA + deskonestudentB + deskonestudentC)
+numberofdesksAllfull= numberofdesksA + numberofdesksB + numberofdesksC
+newaddedfulldeskAll = addedonedeskAll // 2
