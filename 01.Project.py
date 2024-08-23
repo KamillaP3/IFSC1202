386aa6d3a0b94dadc55d7ac79d9f0e0967853fc0
KamillaP3 <kparker3@ualr.edu>
2024-08-23 17:01:13 +0000

    projectsave

---
+++ 01.Project.py
@@ -0,0 +1,8 @@
+lengthindays = int(input("Enter Length of Time in Days: "))
+lengthinyears = lengthindays // 365
+lengthleftoverfromyears = lengthindays % 365
+lengthinweeks = lengthleftoverfromyears // 7
+lengthleftindays = lengthleftoverfromyears % 7
+print("Years: " + str(lengthinyears))
+print("Weeks: " + str(lengthinweeks))
+print("Days: " + 1000str(lengthleftindays))
