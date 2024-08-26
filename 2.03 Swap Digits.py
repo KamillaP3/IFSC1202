a1bf6279afa85ba7fcb9ca58845764af6e4db06f
KamillaP3 <kparker3@ualr.edu>
2024-08-26 15:44:19 +0000

    save

--- 2.03 Swap Digits.py
+++ 2.03 Swap Digits.py
@@ -2,5 +2,6 @@ number = int(input("Enter a Number: "))
 onedigit= (number % 10)
 tendigit= number / 10
 tendigit= int(tendigit % 10)
-print("{}".format(onedigit))
-print("{}".format(tendigit))
+newtendigit = onedigit * 10
+swappednumber = newtendigit + tendigit
+print("{}".format(swappednumber))
