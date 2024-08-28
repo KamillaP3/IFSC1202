ac9c01d9118139f6dcf88cfe06bfb42f130ec0b8
KamillaP3 <kparker3@ualr.edu>
2024-08-28 15:35:40 +0000

    save

---
+++ 03.04 Digits In Ascending Order.py
@@ -0,0 +1,9 @@
+number = int(input("Enter a Number: "))
+hundreds= number // 100
+tens = (number//10) % 10
+ones = number % 10
+
+if hundreds < tens < ones:
+    print("YES")
+else:
+    print("NO")
