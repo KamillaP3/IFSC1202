fb3e8593979a04167912bdf3d0d1c606ccc27d8c
KamillaP3 <kparker3@ualr.edu>
2024-08-28 14:20:53 +0000

    conditionals

--- 02.04 Last Two Digits.py
+++ 02.04 Last Two Digits.py
@@ -1,1 +1,7 @@
-number = int(input("Enter a Number: "))
+number = float(input("Enter a Number: "))
+onedigit= int(number % 10)
+tendigit= number / 10
+tendigit= int(tendigit % 10) 
+tendigit = tendigit *10
+lasttwo=tendigit + 0.0 + onedigit
+print("{:2.1d}".format(tendigit + onedigit))
