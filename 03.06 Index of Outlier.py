f1f5222eb92d1e274be9b49096c1ab6ed95c1c20
KamillaP3 <kparker3@ualr.edu>
2024-08-29 16:44:09 +0000

    save

---
+++ 03.06 Index of Outlier.py
@@ -0,0 +1,10 @@
+firstnumber = int(input("Enter First Number: "))
+secondnumber = int(input("Enter Second Number: "))
+thirdnumber = int(input("Enter Third Number: "))
+
+if firstnumber == thirdnumber:
+    print(2)
+elif firstnumber == secondnumber:
+    print(3)
+else:
+    print(1)
---
+++ 03.07 Leap Year.py
@@ -0,0 +1,6 @@
+yearnumber = int(input("Enter Year: "))
+
+if yearnumber % 4 == 0:
+    print("LEAP YEAR")
+else:
+    print("COMMON YEAR")
