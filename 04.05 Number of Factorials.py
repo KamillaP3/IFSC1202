782619c49b0929964782beabe864625ce3b2d3fb
KamillaP3 <kparker3@ualr.edu>
2024-09-16 14:55:47 +0000

    save

---
+++ 04.05 Number of Factorials.py
@@ -0,0 +1,5 @@
+sum = 0
+N = int(input("Enter Number: "))
+for i in range (1, N + 1):
+    sum += i
+print(sum)
---
+++ 04.06 Squares In a Range.py
@@ -0,0 +1,7 @@
+sum = 1
+a = int(input("Enter A: "))
+b = int(input("Enter B: "))
+
+for i in range (a, b):
+    sum = i**2
+    print(sum)
