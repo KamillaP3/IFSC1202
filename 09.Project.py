25cda0e072e3849c271d2a092a33cc1cc3508e7e
KamillaP3 <kparker3@ualr.edu>
2024-10-31 14:54:32 +0000

    save

--- 09.Project.py
+++ 09.Project.py
@@ -1,17 +1,38 @@
-distances=[]
-distancesfile=open("09.Project Distances.csv")
-x = distancesfile.readline()
-while x != "":
-    y = x.split(",")
-    distances.append(y)
-    x = distancesfile.readline()
-    
+import csv
 
-distancesfile.close()
-for row in distances:
-    for element in row:
-        print(element, end=" ") 
-    print() 
+file = '09.Project Distances.csv'
+with open(file, mode='r') as file:
+    reader = csv.reader(file)
+    distances = [row for row in reader]
 
+print("Distance Table:")
 for row in distances:
-    print("{:>8s} {:>20s} {:>23s}".format(*row[:3]))
+    print(" ".join(f"{item:>10}" for item in row))
+
+fromcity = input("Enter From City: ")
+
+# Prompt for a To City
+tocity = input("Enter To City: ")
+
+# Search the zeroth column for the From City
+fromindex = -1
+for i in range(len(distances)):
+    if distances[i][0] == fromcity:
+        fromindex = i
+        break
+
+# Search the zeroth row for the To City
+toindex = -1
+for j in range(len(distances[0])):
+    if distances[0][j] == tocity:
+        toindex = j
+        break
+
+# Check if the cities were found and display the result
+if fromindex == -1:
+    print("Invalid From City")
+elif toindex == -1:
+    print("Invalid To City")
+else:
+    distance = distances[fromindex][toindex]
+    print(f"{fromcity} to {tocity} - {distance} miles")
